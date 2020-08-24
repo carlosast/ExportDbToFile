4b1753a2440bca85cb36f21d6c2bd0b47e39a96f
carlosast <carlosast@msn.com>
2020-08-24 08:57:53 -0300

    Primeira versão

---
+++ ExportXml.cs
@@ -0,0 +1,120 @@
+﻿using Speed.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Translogic.Modules.Core.Spd;
+using Translogic.Modules.Core.Spd.BLL;
+using Translogic.Modules.Core.Spd.Data;
+using Speed.Common;
+
+namespace ExportarCteXml
+{
+    public class ExportXml
+    {
+        StreamWriter writerOk;
+        StreamWriter writerError;
+        public int Export(string dir, string chaves, string select, ProgressBar progressBar, Label lblMessage)
+        {
+            int count = 0;
+
+            using (writerOk = new StreamWriter(Path.Combine(dir, "_Export_OK.txt"), false))
+            {
+                using (writerError = new StreamWriter(Path.Combine(dir, "_Export_Error.txt"), false))
+                {
+                    using (var db = Dbs.NewDb(Db.Translogic))
+                    {
+                        var _chaves = ParseChaves(chaves);
+                        foreach (var chave in _chaves)
+                        {
+                            var cte = BL_Cte.SelectSingle(db, new Cte { ChaveCte = chave });
+                            if (cte != null)
+                            {
+                                count++;
+                                ExportCte(db, dir, cte);
+                            }
+                        }
+
+                        lblMessage.Text = "Consultando ...";
+                        lblMessage.Update();
+
+                        var ids = GetSelect(select);
+
+                        if (ids.Any())
+                        {
+                            progressBar.Minimum = 0;
+                            progressBar.Maximum = ids.Count;
+
+                            foreach (var id in ids)
+                            {
+                                var cte = BL_Cte.SelectByPk(db, id);
+                                if (cte != null)
+                                {
+                                    progressBar.Value = count;
+                                    progressBar.Update();
+
+                                    count++;
+
+                                    lblMessage.Text = string.Format("Processando {0} de {1}", count, ids.Count);
+                                    lblMessage.Update();
+
+                                    ExportCte(db, dir, cte);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        void ExportCte(Database db, string dir, Cte cte)
+        {
+            var sql = "SELECT ARQUIVO_XML FROM CTE_ARQUIVO WHERE ID_CTE = " + cte.IdCte;
+            string xml = db.ExecuteScalar(sql) as string;
+
+            if (string.IsNullOrWhiteSpace(xml))
+            {
+                LogError(cte.ChaveCte);
+            }
+            else
+            {
+                File.WriteAllText(Path.Combine(dir, cte.ChaveCte + ".xml"), xml, System.Text.UTF32Encoding.UTF8);
+                LogOk(cte.ChaveCte);
+            }
+        }
+
+        List<string> ParseChaves(string chaves)
+        {
+            if (chaves == null || chaves == string.Empty)
+                return new List<string>();
+
+            return chaves.Split(new char[] { '\r', '\n', ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(p => p.Trim()).Distinct(p => p).ToList();
+        }
+
+        List<decimal> GetSelect(string select)
+        {
+            if (select == null || select == string.Empty)
+                return new List<decimal>();
+
+            if (select.EndsWith(";"))
+                select = select.Substring(0, select.Length - 1);
+
+            return Dbs.Execute(Db.Translogic, (db) => db.ExecuteArray1D<decimal>(select)).ToList();
+        }
+
+        void LogOk(string message)
+        {
+            writerOk.WriteLine(message);
+        }
+        void LogError(string message)
+        {
+            writerError.WriteLine(message);
+        }
+
+    }
+
+}
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Speed.Common;
+using Speed.Data;
+using Translogic.Modules.Core.Spd;
+using Translogic.Modules.Core.Spd.BLL;
+using Translogic.Modules.Core.Spd.Data;
+
+namespace ExportarCteXml
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMessage.Visible = progressBar.Visible = false;
+
+            Show();
+
+            Sis.Inicializar(Translogic.Modules.Core.Spd.Data.Enums.EnumSistema.Indefinido, "ExportarCteXml", false);
+
+#if DEBUG
+            txtSelect.Text =
+@"select id_cte from translogic.cte c where
+c.data_cte between to_Date('01/08/2019 00:00:00','dd/mm/yyyy hh24:mi:ss')
+and to_Date('31/07/2020 23:59:59','dd/mm/yyyy hh24:mi:ss')
+and c.sigla_uf_ferrovia in ('MT','MS','SP')
+AND C.SITUACAO_ATUAL = 'AUT'";
+
+            txtDirectory.Text = @"D:\temp\ExportCtes";
+
+            //btnExportar_Click(null, null);
+#endif
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dir = txtDirectory.Text.Trim();
+
+                if (dir == string.Empty)
+                    throw new Exception("Entre o diretório");
+
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+
+                string chaves = txtChaves.Text.Trim();
+                string select = txtSelect.Text.Trim();
+
+                if (chaves == string.Empty && select == string.Empty)
+                    throw new Exception("Entre a lista de chaves ou o select");
+
+                if (chaves != string.Empty && select != string.Empty)
+                    throw new Exception("Entre as chaves ou o select. Não ambos ao mesmo tempo");
+
+                var exp = new ExportXml();
+                DateTime date = DateTime.Now;
+
+                lblMessage.Visible = progressBar.Visible = true;
+                int count = exp.Export(dir, chaves, select, progressBar, lblMessage);
+
+                string msg = "Exportados " + count + " arquivos. Tempo: " + DateTime.Now.Subtract(date);
+                MessageBox.Show(msg, "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            lblMessage.Visible = progressBar.Visible = false;
+        }
+
+        private void picHelp_Click(object sender, EventArgs e)
+        {
+            string msg =
+@"
+Entre um select com o campo que deseja exportar.
+    ex: select coluna_com_arquivo from minha_tabela where .... order by ....
+Se deseja ao exportar ter controle sobre o nome do arquivo, retorne na primeira coluna o nome do arquivo e na segunda coluna o campo à ser exportado
+    ex: select 'XML_' || coluna_com_id, coluna_com_arquivo from minha_tabela where .... order by ....
+";
+        }
+    }
+
+}
